a737fc580811c5217a6c4960730ce71778c45cab
thatnoobgamer <ybarve@outlook.com>
2025-02-08 15:49:51 -0800

    added weather support among other stuff

--- main.py
+++ main.py
@@ -120,16 +120,33 @@ def progress():
 @app.route('/weather')
 def weather():
     # Example API for weather (replace with actual API and key)
-    api_key = "your_openweather_api_key"
+    api_key = "68c62c52de388d389eb3f205e9e7b45c"
     location = "London"
     weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}"
 
     response = requests.get(weather_url).json()
+    print(response)
 
     temperature = response['main']['temp'] - 273.15  # Convert from Kelvin to Celsius
     weather_desc = response['weather'][0]['description']
 
     return render_template('weather.html', temperature=temperature, weather_desc=weather_desc)
+
+@app.route('/get_weather')
+def get_weather():
+    # Get latitude and longitude from the query parameters
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    API_KEY = "68c62c52de388d389eb3f205e9e7b45c"
+    # Construct the URL for OpenWeatherMap API
+    url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric'
+
+    # Make the API request to OpenWeatherMap
+    response = requests.get(url)
+    weather_data = response.json()
+
+    # Return the weather data as JSON
+    return jsonify(weather_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
